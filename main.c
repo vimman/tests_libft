1af7a89adf71b74fbf019b0ec2ad57518f1aadfa
Quentin DUROT <qdurot@.42.fr>
2017-04-13 19:14:23 +0200

    main till strlcat

--- main.c
+++ main.c
@@ -8,27 +8,68 @@ int	main(int argc, char **argv)
 		printf("Wrong number of arguments");
 		return (1);
 	}
-	else if (strcmp(argv[1], "memset"))
-	{
-		printf("REAL = %s", (char *)memset(argv[2], atoi(argv[3]), atoi(argv[4])));
-		printf("YOU = %s", (char *)ft_memset(argv[2], atoi(argv[3]), atoi(argv[4])));
-	}
-	else if (strcmp(argv[1], "bzero"))
-	{
-		char str[] = "J'ai la belle vie en Normandie";
-		char str1[] = "J'ai la belle vie en Normandie";
+	//else if (strcmp(argv[1], "memset"))
+	//{
+	//	printf("REAL = %s\n", (char *)memset(argv[2], atoi(argv[3]), atoi(argv[4])));
+	//	printf("YOU = %s\n", (char *)ft_memset(argv[2], atoi(argv[3]), atoi(argv[4])));
+	//}
+	//else if (strcmp(argv[1], "bzero"))
+	//{
+	//	char str[] = "J'ai la belle vie en Normandie";
+	//	char str1[] = "J'ai la belle vie en Normandie";
 
-		bzero(str, (size_t)argv[2]);
-		ft_bzero(str1, (size_t)argv[2]);
+	//	bzero(str, (size_t)argv[2]);
+	//	ft_bzero(str1, (size_t)argv[2]);
 
-		printf("REAL = %s", str);
-		printf("YOU = %s", str1);
-	}
-	else if (strcmp(argv[1], "memcpy"))
+	//	printf("REAL = %s\n", str);
+	//	printf("YOU = %s\n", str1);
+	//}
+	//else if (strcmp(argv[1], "memcpy"))
+	//{
+	//	printf("REAL = %s\n", memcpy((void *)argv[2], (void *)argv[3], atoi(argv[4])));
+	//	printf("YOU = %s\n", ft_memcpy((void *)argv[2], (void *)argv[3], atoi(argv[4])));
+	//}
+	else if (atoi(argv[1]) == 8)
 	{
-		printf("REAL = %s", memcpy((void *)argv[2], (void *)argv[3], atoi(argv[4])));
-		printf("YOU = %s", ft_memcpy((void *)argv[2], (void *)argv[3], atoi(argv[4])));
+		printf("REAL = %zu\n", strlen(argv[2]));
+		printf("YOU = %zu\n", ft_strlen(argv[2]));
 	}
+	else if (atoi(argv[1]) == 9)
+	{
+		printf("REAL = %s\n", strdup(argv[2]));
+		printf("YOU = %s\n", ft_strdup(argv[2]));
+	}
+
+	else if (atoi(argv[1]) == 10)
+	{
+		printf("REAL = %s\n", strcpy(argv[3], argv[2]));
+		printf("YOU = %s\n", ft_strcpy(argv[3], argv[2]));
+	}
+
+	else if (atoi(argv[1]) == 11)
+	{
+		printf("REAL = %s\n", strncpy(argv[3], argv[2], atoi(argv[4])));
+		printf("YOU = %s\n", ft_strncpy(argv[3], argv[2], atoi(argv[4])));
+	}
+
+	else if (atoi(argv[1]) == 12)
+	{
+		printf("REAL = %s\n", strcat(argv[3], argv[2]));
+		printf("YOU = %s\n", ft_strcat(argv[3], argv[2]));
+	}
+
+	else if (atoi(argv[1]) == 13)
+	{
+		printf("REAL = %s\n", strncat(argv[3], argv[2], atoi(argv[4])));
+		printf("YOU = %s\n", ft_strncat(argv[3], argv[2], atoi(argv[4])));
+	}
+
+	else if (atoi(argv[1]) == 14)
+	{
+		printf("REAL = %zu\n", strlcat(argv[3], argv[2], atoi(argv[4])));
+		printf("YOU = %zu\n", ft_strlcat(argv[3], argv[2], atoi(argv[4])));
+	}
+
 	else
 		printf("Il faut arreter de boire tres cher...");
 	return (0);
