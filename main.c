d6afefd7fecb3990479bbb3e3f24bdd235fc276d
Quentin DUROT <qdurot@.42.fr>
2017-04-15 01:37:54 +0200

    on en ai la

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include "libft.h"
 
@@ -38,11 +39,48 @@ int	main(int argc, char **argv)
 		printf("REAL = %s\n", &str[i]);
 		printf("YOU = %s\n", &str1[i]);
 	}
-//	else if (atoi(argv[1]) == 3)
+	else if (atoi(argv[1]) == 3)
+	{
+		char	test[100] = "voila mes boules";
+		ft_memcpy(test+5, test, strlen(test)+1);
+		printf("%s", test);
+//		char	*tmp;
+//		char	*tmp2;
+//
+//		tmp = strdup(argv[2]);
+//		tmp2 = &argv[2][1];
+//		printf("REAL = %s\n", memcpy(tmp2, tmp, atoi(argv[3])));
+//		tmp = strdup(argv[2]);
+//		tmp2 = &argv[2][1];
+//		printf("YOU = %s\n", ft_memcpy(tmp2, tmp, atoi(argv[3])));
+	}
+
+//	else if (atoi(argv[1]) == 4)
 //	{
-//		printf("REAL = %s\n", memcpy((void *)argv[2], (void *)argv[3], atoi(argv[4])));
-//		printf("YOU = %s\n", ft_memcpy((void *)argv[2], (void *)argv[3], atoi(argv[4])));
+//		char	*tmp;
+//
+//		tmp = strdup(argv[3]);
+//		printf("REAL = %s\n", memccpy(argv[2], argv[3], atoi(argv[4])));
+//		printf("YOU = %s\n", ft_memccpy(argv[2], tmp, atoi(argv[4])));
+//		free(tmp);
 //	}
+
+	else if (atoi(argv[1]) == 5)
+	{
+		char	*tmp;
+		char	*tmp2;
+
+		tmp = strdup(argv[2]);
+		tmp2 = &argv[2][1];
+		//printf("tmp = %p\n", tmp);
+		//printf("tmp2 = %p\n", tmp2);
+		printf("REAL = %s\n", memmove(tmp2, tmp, atoi(argv[3])));
+		tmp = strdup(argv[2]);
+		tmp2 = &argv[2][1];
+		printf("YOU = %s\n", ft_memmove(tmp2, tmp, atoi(argv[3])));
+
+	}
+
 	else if (atoi(argv[1]) == 8)
 	{
 		printf("REAL = %zu\n", strlen(argv[2]));
@@ -83,7 +121,20 @@ int	main(int argc, char **argv)
 		printf("REAL = %zu\n", strlcat(argv[3], argv[2], atoi(argv[4])));
 		printf("YOU = %zu\n", ft_strlcat(argv[3], argv[2], atoi(argv[4])));
 	}
-
+	else if (atoi(argv[1]) == 21)
+	{
+		printf("REAL = %d\n", atoi(argv[2]));
+		printf("YOU = %d\n", ft_atoi(argv[2]));
+	}
+//	else if (atoi(argv[1]) == 22)
+//	{
+//		int	i;
+//
+//		i =  - 10;
+//		while (i < 255)
+//		{
+//			
+//
 	else
 		printf("Il faut arreter de boire tres cher...");
 	return (0);
